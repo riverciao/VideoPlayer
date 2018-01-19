3ceab1ce4934132d3d68f33573f5df001656ae68
Ciao Huang <riverciao@gmail.com>
2018-01-19 13:35:10 +0800

    add VideoPlayerViewController

---
+++ VideoPlayer/VideoPlayer/VideoPlayerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerViewController.swift
+//  VideoPlayer
+//
+//  Created by riverciao on 2018/1/19.
+//  Copyright © 2018年 riverciao. All rights reserved.
+//
+
+import UIKit
+
+class VideoPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
